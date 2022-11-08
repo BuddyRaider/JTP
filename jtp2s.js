2ec630cc5de2ef0a6b60e14aec7d9fb48a7714e4
BuddyRaider <104165073+BuddyRaider@users.noreply.github.com>
2022-11-08 16:05:35 +0000

    CORS Changes

--- jtp2s.js
+++ jtp2s.js
@@ -14,6 +14,18 @@ class JTP2 {
 	run(port=443, call=() => {}) {
 		this.server.on("request", (req, res) => {
 			//console.log(req.headers);
+			if (req.method === "OPTIONS") {
+				res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+				res.end();
+			}
+			try {
+				res.setHeader("Access-Control-Allow-Headers", req.headers.origin);
+				res.setHeader("Conetent-Type", "text/plain");
+			} catch {
+				res.end();
+				return;
+			}
+			console.log(res.headers);
 			if (req.headers["jtp-evt"] !== undefined) {
 				this.evts[req.headers["jtp-evt"]](req, res);
 				try {
