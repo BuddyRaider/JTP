eb9f912eb165014fc4ca153bcd56b3acc4486e58
BuddyRaider <104165073+BuddyRaider@users.noreply.github.com>
2022-11-08 12:26:39 -0500

    Update index.js

--- CAT/client/index.js
+++ CAT/client/index.js
@@ -1,1 +1,6 @@
+const j2c = require("../../../jtp2c.js");
+const client = new j2c("CAT.BuddyDev.repl.co", true);
 
+client.request({"jtp-evt": "home"}, (res) => {
+  console.log(res.headers["jtp-body"]);
+});
