a74be65cd448df3f86234fe59e48c5dfd81f3af1
BuddyRaider <104165073+BuddyRaider@users.noreply.github.com>
2022-11-09 08:39:05 -0500

    Update index.js

--- CAT/server/index.js
+++ CAT/server/index.js
@@ -2,8 +2,8 @@ const j2s = require("../../../jtp2s.js");
 const server = new j2s();
 
 server.on("home", () => {
-  res.writeHead(200, {"jtp-body": {"text": "Hello World!"}});
-  res.end();
+	res.writeHead(200, {"jtp-body": {"text": "Hello World!"}});
+	res.end();
 });
 
 server.run(443, () => {console.log("Server Running On Port 443!")});
